d3fec26bff2ca8c35e99d2786ac0b17eaba60696
Dmytro-1506 <kyunlik@gmail.com>
2022-12-07 23:17:08 +0100

    simple complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryItemsArr = [];
+
+galleryItems.map(element => {
+    const galleryItem = `<div class="gallery__item"><a class="gallery__link" href=${element.original}>
+    <img
+      class="gallery__image"
+      src=${element.preview}
+      data-source=${element.original}
+      alt=${element.description}
+    />
+  </a></div>`;
+    galleryItemsArr.push(galleryItem);
+})
+
+galleryList.innerHTML = galleryItemsArr.join('');
+
+galleryList.addEventListener('click', (event) => {
+    event.preventDefault();    
+})
+
+let gallery = new SimpleLightbox('.gallery a');
