cec7128d33ba5f663bcfa3571ba8d9eab7aa3446
Dmytro-1506 <kyunlik@gmail.com>
2022-12-08 23:04:08 +0100

    video player complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    
+    let currentTime = data.seconds;
+
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+}
+
+let savedTime = localStorage.getItem("videoplayer-current-time");
+    if (savedTime) {
+        player.setCurrentTime(Number(savedTime));
+    }
+
+player.on('timeupdate', throttle(onPlay, 1000));
 
